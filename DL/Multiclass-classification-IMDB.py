959bb7b01b008fd4686f3cfcd6e2efb8ce9b9193
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 23:01:30 +0800

    Update Multiclass-classification-IMDB.py

--- DL/Multiclass-classification-IMDB.py
+++ DL/Multiclass-classification-IMDB.py
@@ -62,3 +62,22 @@ plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+print('###### Restart Train ######')
+model.fit(x_train,
+          y_train,
+          epochs=4,
+          batch_size=512)
+
+result = model.evaluate(x_test, y_test)
+
+print(result)
